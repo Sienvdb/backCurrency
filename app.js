2ff71f7881803ffbc0790ad45c5e3616703df212
Sienvdb <sienvdbergh@gmail.com>
2022-04-28 11:51:05 +0200

    feat: made route

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var transferRouter = require('./routes/transfer');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/api/v1/transfers', transferRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
