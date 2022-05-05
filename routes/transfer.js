1181686aebcfdcb411c92d0b5ed41b1bc37929c5
MarieSerroyen <marie.serroyen@gmail.com>
2022-05-05 11:39:45 +0200

    feat: made index.js in routes folder

--- routes/transfer.js
+++ routes/transfer.js
@@ -5,5 +5,6 @@ const transfersController = require("./../controllers/transfers");
 router.get("/", transfersController.getAll);
 router.post("/", transfersController.create);
 router.get("/:id", transfersController.getId);
+router.get("/leaderboard", transfersController.getCoins);
 
 module.exports = router;
