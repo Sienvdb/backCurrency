18cbf02cade9cbfec503dc0b331dd6195c617303
MarieSerroyen <marie.serroyen@gmail.com>
2022-05-05 12:25:00 +0200

    feat: require mongoose in app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+const mongoose = require('mongoose');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
