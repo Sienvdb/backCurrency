bb6dbd27009a9df549d4b0fc8aee1fce5c9e301d
MarieSerroyen <marie.serroyen@gmail.com>
2022-05-05 12:23:05 +0200

    feat: put mongoose connection in transfer.js

--- models/transfer.js
+++ models/transfer.js
@@ -1,1 +1,12 @@
 //make connection with mongoDB
+const mongoose = require('mongoose');
+    const Schema = mongoose.Schema;
+
+    const transfersSchema = new Schema({
+    username:  String, // String is shorthand for {type: String}
+    message: String
+});
+
+const Transfer = mongoose.model('Message', transfersSchema);
+
+module.exports = Transfer;
