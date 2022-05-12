a52992e2d66af36e48241f20f3c2369e01fa8e4c
Sienvdb <sienvdbergh@gmail.com>
2022-05-12 10:13:22 +0200

    feat: make connection with monogDb (getSingleTransfer)

--- controllers/transfer.js
+++ controllers/transfer.js
@@ -1,18 +1,30 @@
+let Transfer = require ("../models/transfer")
+
 const getSingleTransfer = (req, res) => {
-    const response = {
-        status: "succes",
-        data:{
-            messages: [
-                {
-                    "sender" : "person",
-                    "receiver": "person",
-                    "coins" : "number of coins",
-                    "message" : "reason"
-                }
-            ]
+Transfer.find({ "sender": 'sien'}, function (err, docs) {
+    if (err){
+        const response = {
+            status: "error",
+            data:{
+                transfer: docs
+            }        
         }
+        res.json(response);
     }
-    res.json(response);
+    else{
+        const response = {
+            status: "success",
+            data:{
+                transfer: docs
+            }        
+        }
+        console.log("First function call : ", docs);
+        res.json(response);
+
+    }
+
+});
+
 }
 
 module.exports.getSingleTransfer = getSingleTransfer;
