33dcf7ae5c8275f253ff2fb907d624425e8eb4af
Sienvdb <sienvdbergh@gmail.com>
2022-05-12 09:24:14 +0200

    feat: make user schema

---
+++ models/user.js
@@ -0,0 +1,15 @@
+//make connection with mongoDB
+const mongoose = require('mongoose');
+    const Schema = mongoose.Schema;
+
+    const userSchema = new Schema({
+        firstname: String,
+        lastname: String,
+        username: String,
+        email: String,
+        coins: Number
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
