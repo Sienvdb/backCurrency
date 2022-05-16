5632cce86e78d86c97958da457150cb7cfb7d8ac
Sienvdb <sienvdbergh@gmail.com>
2022-05-16 14:31:20 +0200

    feat: add token to login

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,7 @@ const User = require("../models/user");
 const bcrypt = require("bcrypt");
 const { token } = require("morgan");
 const jwt = require("jsonwebtoken");
+const passport = require('passport');
 
 const verification = (req, res) => {
     const response = {
@@ -74,27 +75,80 @@ const signup =  async (req, res) => {
 const login = async (req, res) => {
     const body = req.body;
     const user = await User.findOne({email: body.email});
-    
+    console.log("ok1");
     if(user) {
+        //check if user password and hashed password are the same
         const validatePassword = await bcrypt.compare(body.password, user.password);
+        console.log("ok2");
 
         if(validatePassword) {
+            console.log("ok3");
+            let token = jwt.sign({
+                uid: user._id, 
+                username: user.username
+            }, "SecretWord");
+        
+        
             res.json({
                 status: "success",
-                message: "Valid password"
+                token: token
             });
         } else {
+            console.log("ok4");
+
             res.json({
                 status: "error",
-                message: "Wrong password"
+                message: "Password is incorrect"
             });
         }
     } else {
+        console.log("ok5");
+
         res.json({
             status: "error",
             message: "No user found with this email"
         });
     }
+}
+
+const login2 = async (req, res) => {
+    const body= req.body;
+    const user = await User.authenticate()(req.body.username, req.body.password).then(result => {
+        console.log(User);
+        console.log(result);
+        console.log(result.user);
+        console.log(req.body.username);
+        console.log(req.body.password);
+        if(result.user) {    
+            const validatePassword = bcrypt.compare(body.password, user.password);
+            console.log(validatePassword);
+
+            if(validatePassword) {
+                let token = jwt.sign({
+                    uid: result.user._id, 
+                    username: result.user.username
+                }, "SecretWord");
+    
+                return res.json({
+                    status: "success",
+                    data: {
+                        "token": token
+                    }
+                });
+            } else {
+                res.json({
+                    status: "failed",
+                    message: "Wrong password"
+                });
+            }
+        } else {
+            res.json({
+                status: "failed",
+                message: "No user found with this email"
+            });
+        }    
+    });
+    
 }
 
 
