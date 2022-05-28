e3823c2bb67a946ba43b439ee8c356cc2debd4f8
MarieSerroyen <marie.serroyen@gmail.com>
2022-05-28 15:41:54 +0200

    feat: added pagination

--- routes/transfers.js
+++ routes/transfers.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('./../passport/passport');
+const page = require('./../middleware/pagination');
 const transfersController = require("./../controllers/transfers");
 
 router.get("/transfers", transfersController.getAllTransfersByToken);
+router.get("/transfers/paginate", page.paginatedResults(transfers), transfersController.getAllTransfersByToken);
 router.post("/transfers", transfersController.create);
-router.get("/transfers/:id", transfersController.getTransferId);
+router.get("/transfers", transfersController.getTransferId);
 router.get("/getCoins", passport.authenticate('jwt', {session: false}), transfersController.getCoins);
 
 module.exports = router;
