953330b87bc63facd2e10342a2f3b6d518c8087c
Sienvdb <sienvdbergh@gmail.com>
2022-05-28 15:46:06 +0200

    feat: make first primus connection

---
+++ primus/primus.js
@@ -0,0 +1,10 @@
+const Primus = require('primus');
+
+let go = (server) => {
+    let primus = new Primus(server, {});
+    primus.on('connection', (spark) => {
+        console.log("yoepie");
+    })
+}
+
+module.exports.go = go;
