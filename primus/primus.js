2ac4c04a76c56b31fcb9526d63dcc902eeda4f54
Sienvdb <sienvdbergh@gmail.com>
2022-05-28 20:07:48 +0200

    feat: try solving primusbug

--- primus/primus.js
+++ primus/primus.js
@@ -1,14 +1,14 @@
 const Primus = require('primus');
 
 let go = (server) => {
-    let primus = new Primus("http://localhost:3000/index.html", {});
+    let primus = new Primus(server, {});
     //primus.save(__dirname +'/primuslib.js')
 
     primus.on('connection', (spark) => {
         console.log("yoepie");
-        spark.on("data", (data) => {
+        /*spark.on("data", (data) => {
             primus.write(data);
-        })
+        })*/
     })
 }
 
